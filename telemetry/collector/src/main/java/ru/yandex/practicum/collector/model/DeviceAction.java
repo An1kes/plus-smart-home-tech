092f371a43140fb8ccce81b4197d373821d5883b
Vlad <kichatov.vlad@yandex.ru>
2026-09-09 01:56:10 +0500

    fix public rep

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/DeviceAction.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/DeviceAction.java
@@ -10,5 +10,5 @@ import lombok.ToString;
 public class DeviceAction {
     private String sensorId;
     private ActionType type;
-    private Integer value; // Также Integer, так как может быть null
+    private Integer value;
 }
